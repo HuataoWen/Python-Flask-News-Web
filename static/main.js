8f6c68fd5cd8116ae5cde81a37761ace6c042c22
HuataoWen421 <60019549+HuataoWen421@users.noreply.github.com>
2020-02-22 15:32:20 -0800

    word cloud revise

--- static/main.js
+++ static/main.js
@@ -14,36 +14,38 @@ function searchNewsPage() {
 }
 */
 
-
-
 function run() {
     // List of words
-    var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
-    
+    var myWords = [{word: "Running", size: "2"},
+    {word: "Surfing", size: "20"},
+    {word: "Climbing", size: "50"},
+    {word: "Kiting", size: "30"},
+    {word: "Sailing", size: "20"},
+    {word: "Snowboarding", size: "60"} ]
+
     // set the dimensions and margins of the graph
-    var margin = {top: 10, right: 10, bottom: 10, left: 10},
-        width = 150 - margin.left - margin.right,
-        height = 150 - margin.top - margin.bottom;
-    
+    var margin = {top: 1, right: 1, bottom: 1, left: 1},
+    width = 200 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
     // append the svg object to the body of the page
     var svg = d3.select("#wordsCloud").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
+
     // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
     // Wordcloud features that are different from one word to the other must be here
     var layout = d3.layout.cloud()
     .size([width, height])
-    .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
-    .padding(5)        //space between words
+    .words(myWords.map(function(d) { return {text: d.word, size: d.size}; }))
+    .padding(0) //space between words
     .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .fontSize(function(d) { return d.size; }) // font size of words
+    .fontSize(function(d) { return d.size; })      // font size of words
     .on("end", draw);
-    
     layout.start();
-    
+
     // This function takes the output of 'layout' above and draw the words
     // Wordcloud features that are THE SAME from one word to the other can be here
     function draw(words) {
@@ -57,9 +59,7 @@ function run() {
         .style("fill", "#69b3a2")
         .attr("text-anchor", "middle")
         .style("font-family", "Impact")
-        .attr("transform", function(d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
+        .attr("transform", function(d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; })
         .text(function(d) { return d.text; });
     }
 }
