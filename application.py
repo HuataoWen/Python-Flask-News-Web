72de4e854af8faa7600736d0ce695956986a58b0
HuataoWen421 <60019549+HuataoWen421@users.noreply.github.com>
2020-03-06 15:30:28 -0800

    init release

--- application.py
+++ application.py
@@ -1,23 +1,45 @@
 from flask import Flask, redirect, url_for, render_template, request, abort, jsonify
 from newsapi import NewsApiClient
-from newsapi.newsapi_exception import NewsAPIException
 
 app = Flask(__name__)
+
+def newsFilter(data):
+   tmp = {}
+   for k, v in data.items():
+      tmp[k] = v
+      if k == 'articles':
+         tmp[k] = []
+         for item in data[k]:
+            if item['author'] == None or\
+               item['source'] == None or\
+               item['description'] == None or\
+               item['title'] == None or\
+               item['url'] == None or\
+               item['urlToImage'] == None or\
+               item['publishedAt'] == None:
+               continue
+            tmp[k].append(item)
+   return tmp
+
 @app.route('/')
 def index():
+   # Init
    newsapi = NewsApiClient(api_key='c2174dfb402940cab6d1829c14225861')
 
-   top_headlines = newsapi.get_top_headlines(q='', sources='cnn', language='en')
+   # /v2/top-headlines
+   top_headlines = newsapi.get_top_headlines(q='', sources='', language='en')
+   
+   # Word cloud
    wordFreq = {}
    tmp = ''
    for i in range(len(top_headlines['articles'])):
       tmp += top_headlines['articles'][i]['title']
    tmp = tmp.split()
 
+   # Remove stopwords
    file1 = open("stopwords_en.txt", "r")
    stopwords_en = file1.read().splitlines()
    file1.close()
-
    for word in tmp:
       if word not in stopwords_en:
          if word in wordFreq:
@@ -25,35 +47,34 @@ def index():
          else:
             wordFreq[word] = 1
    wordFreq = sorted(wordFreq.items(), key=lambda kv: kv[1], reverse=True)
-   #print(wordFreq)
-
-
-   cnn_top_headlines = newsapi.get_top_headlines(q='', sources='cnn', language='en')
-   #print(cnn_top_headlines)
-   fox_news_top_headlines = newsapi.get_top_headlines(q='', sources='fox-news', language='en')
+   top_headlines = newsFilter(top_headlines)
    
-   headlines = {'top_headlines':top_headlines,\
-      'cnn_top_headlines':cnn_top_headlines,\
-      'fox_news_top_headlines':fox_news_top_headlines,\
-      'wordFreq':wordFreq}
-   #print((top_headlines))
+   # /v2/top-headlines
+   cnn_top_headlines = newsapi.get_top_headlines(q='', sources='cnn', language='en')
+   cnn_top_headlines = newsFilter(cnn_top_headlines)
+
+   # /v2/top-headlines
+   fox_news_top_headlines = newsapi.get_top_headlines(q='', sources='fox-news', language='en')
+   fox_news_top_headlines = newsFilter(fox_news_top_headlines)
+
+   # Combine all headlines
+   headlines = {\
+      'top_headlines': top_headlines,\
+      'cnn_top_headlines': cnn_top_headlines,\
+      'fox_news_top_headlines': fox_news_top_headlines,\
+      'wordFreq': wordFreq}
+
    return render_template('home.html', headlines = headlines)
 
 @app.route('/search', methods=['GET', 'POST'])
 def searchNews():
-   """
-   print(request.form['keyword'])
-   print(request.form['fromDate'])
-   print(request.form['toDate'])
-   print(request.form['category'])
-   print(request.form['source'])
-
-   """
    q = request.form['keyword']
    sources = '' if request.form['source'] == "all" else request.form['source']
    from_param = request.form['fromDate']
    to = request.form['toDate']
+
    newsapi = NewsApiClient(api_key='c2174dfb402940cab6d1829c14225861')
+
    try:
       all_articles = newsapi.get_everything(\
          q=q,\
@@ -63,30 +84,45 @@ def searchNews():
          language='en',\
          sort_by='publishedAt',\
          page=5)
-      print(type(all_articles))
       #print(all_articles)
+      all_articles = newsFilter(all_articles)
       return jsonify(all_articles)
    except Exception as e:
-      #print(e)
-      errorStr = str(e)
-      errorStr = errorStr.strip("{'").strip("'}").split("', '")
+      #print(str(e))
+      errorStr = str(e).strip("{'").strip("'}").strip('"}').split("', '")
+      #print("errorStr----------------------")
+      #print(errorStr)
+      #print("errorStr----------------------")
       error = {}
       for item in errorStr:
+         #print("item----------------------")
+         #print(item)
+         #print("item----------------------")
          t = item.split("': '")
+         if len(t) == 1:
+            t = t[0].split("'"+': "')
+         #print("t----------------------")
+         #print(t)
+         #print("t----------------------")
          error[t[0]] = t[1]
-      #print(error)
       return jsonify(error)
-
 
 @app.route('/requestSource', methods=['GET', 'POST'])
 def requestSource():
    newsapi = NewsApiClient(api_key='c2174dfb402940cab6d1829c14225861')
-   sources = newsapi.get_sources(category=request.form['category'], country='us', language='en')
-   #print(sources['sources'])
+
+   #print("--------------------------")
+   #print(request.form['category'])
+   #print("--------------------------")
+
+   if request.form['category'] == "all":
+      sources = newsapi.get_sources(country='us', language='en')
+   else:
+      sources = newsapi.get_sources(category=request.form['category'], country='us', language='en')
+
    result = []
    for i in range(len(sources['sources'])):
       result.append(sources['sources'][i]['name'])
-      #print(sources['sources'][i]['name'])
 
    return jsonify(result)
 
